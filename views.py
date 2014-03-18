b08cbc8b8bd9f1faebcf2b94a652470a8d794d5d
James Gissendaner <jamestgissendanerjr@gmail.com>
2014-03-18 06:14:03 +0000

    Dashboard

--- views.py
+++ views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, render_to_response
+from archiver.models import ArchiverApp
 
 from archiver.decorators import dashboard_view
 # Create your views here.
 @dashboard_view
 def dashboard(request, context):
+    context['creators'] = ArchiverApp.objects.get(app_label='louisville_crime').creators.all()
+    context['members'] = ArchiverApp.objects.get(app_label='louisville_crime').members.all()
     return render_to_response('louisville_crime/dashboard.html', context)
