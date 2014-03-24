e79bb09a402488d1014a259bdfd3623a0bf2a458
James Gissendaner <jamestgissendanerjr@gmail.com>
2014-03-24 21:20:27 +0000

    Dashboard and app information changes

---
+++ __init__.py
@@ -0,0 +1,20 @@
+from archiver.models import ArchiverApp
+
+app_info = {
+    'name':'Louisville Crime',
+    'app_label' : 'louisville_crime', 
+    'flavor_text' : "Activity of the 5-0 in 502!",
+    'description' : "Louisville Crime is a project dedicated to storing a record the crime data for Metro Louisville.",
+    'public' : False,
+    'github' : "jgissend10/louisville_crime"
+}
+
+try:
+    app = ArchiverApp.objects.get(app_label=app_info['app_label'])
+    for key, value in app_info.iteritems():
+        setattr(app, key, value)
+    app.save()
+except ArchiverApp.DoesNotExist:
+    app = ArchiverApp(**app_info)
+    app.save()
+
--- views.py
+++ views.py
@@ -3,8 +3,6 @@ from archiver.models import ArchiverApp
 
 from archiver.decorators import dashboard_view
 # Create your views here.
-@dashboard_view
+@dashboard_view('louisville_crime')
 def dashboard(request, context):
-    context['creators'] = ArchiverApp.objects.get(app_label='louisville_crime').creators.all()
-    context['members'] = ArchiverApp.objects.get(app_label='louisville_crime').members.all()
     return render_to_response('louisville_crime/dashboard.html', context)
